fc26f5f11673b039e7e6c61b88600b7d5ce44e09
Mayank Kushal <mayankkushal26@gmail.com>
2017-01-25 14:54:29 +0530

    Update settings.py

--- mini_twitter_project/settings.py
+++ mini_twitter_project/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/mini_twitter_project/assets/'
+STATIC_ROOT = '/assets/'
 STATICFILES_DIRS = [STATIC_DIR, ]
 
 
